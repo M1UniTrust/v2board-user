9be1d5cba6b8bd1ffb18fc5f82479b8ed76acea0
M1UniTrust <m15055510xx@gmail.com>
2024-09-01 02:38:26 +0800

    Add files via upload

---
+++ env.example.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: 'UniTrust',
+  // 站点描述
+  description: '邮件收不到检查垃圾邮件',
+  // API
+  host: '',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'black'
+  },
+  // 背景
+  background_url: '',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
